e42b739358203bc07bd2e9800920eeb5130f92df
Ukasyah Nata <ukasyahnata@outlook.com>
2024-08-26 08:41:18 +0700

    .....

--- src/index.js
+++ src/index.js
@@ -17,7 +17,9 @@ const bot = new TelegramBot(token, { polling: true });
 })();
 
 bot.onText(/\/info (\d+)/, async (msg, match) => {
+  // Get the chat ID and the message thread ID
   const chatId = msg.chat.id;
+  const threadId = msg.message_thread_id;
   const site = `${match[1]}`;
 
   try {
@@ -34,18 +36,21 @@ bot.onText(/\/info (\d+)/, async (msg, match) => {
         return info;
       }).join("\n");
 
-      bot.sendMessage(chatId, siteInfo);
+      // Use threadId in the sendMessage call
+      bot.sendMessage(chatId, siteInfo, { message_thread_id: threadId });
     } else {
-      bot.sendMessage(chatId, "No site found with the given ID.");
+      bot.sendMessage(chatId, "No site found with the given ID.", { message_thread_id: threadId });
     }
   } catch (error) {
     bot.sendMessage(
       chatId,
-      "An error occurred while fetching the site information."
+      "An error occurred while fetching the site information.",
+      { message_thread_id: threadId }
     );
     console.error(error);
   }
 });
+
 
 const fetchSite = async (site_id) => {
   const data = await Sites.findAll({
